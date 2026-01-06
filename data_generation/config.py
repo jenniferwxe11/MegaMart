05afc87983878bb77ce37510be9eb189df0062a8
Jennifer Wang Xue'Er <jenniferwxe@gmail.com>
2026-01-07 05:03:39 +0800

    feat[data-gen]: MEGAMART-3 customer data generator
    - sample size customer data generated
    - customer segmentation reflected for non walk-in customers
    - online and in-store customer types
    - realistic communication consent behaviour
    - captures key identity, demographic, location, and onboarding set in the year 2024

---
+++ data_generation/config.py
@@ -0,0 +1,24 @@
+NUM_CUSTOMERS = 100
+
+MONTH_WEIGHTS_2024 = {
+    1: 0.5,
+    2: 0.6,
+    3: 0.7,
+    4: 0.8,
+    5: 0.9,
+    6: 1.0,
+    7: 1.1,
+    8: 1.2,
+    9: 1.3,
+    10: 1.4,
+    11: 1.8,  # 11.11
+    12: 2.0,  # 12.12 + Christmas
+}
+
+TARGET_SEGMENT = {
+    "New Customers": 0.2,
+    "Active Customers": 0.5,
+    "Churn Risk Customers": 0.1,
+    "High Spenders": 0.1,
+    "Budget Shoppers": 0.1,
+}
