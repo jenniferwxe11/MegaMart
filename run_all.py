591cf2ce19ba33c11787da3e149e79d5152841b6
Jennifer Wang Xue'Er <jenniferwxe@gmail.com>
2026-06-10 22:33:42 +0800

    feat[pipeline]: add orchestrator to run generation and profiler

---
+++ run_all.py
@@ -0,0 +1,8 @@
+from data_generation.run_generation import run_generation
+from dirty_data_generation.run_dirty_generation import run_dirty_generation
+from dirty_data_profiling.run_profiling import run_profiling
+
+if __name__ == "__main__":
+    run_generation()
+    run_dirty_generation()
+    run_profiling()
